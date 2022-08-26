650a2786682d0770fd662c4ae89d0761d7f8a950
Nixonsama <samanixonn@gmail.com>
2022-08-26 01:08:33 +0000

    my 2nd commit

---
+++ pet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+petnames() {
+echo "enter the petnames of your pet"
+read petnames
+sudo useradd $petnames
+id $petnames
+sudo tail /etc/passwd
+
+}
+echo "creating petname bingo"
+petnames
+echo "creating petname lion"
+petnames
