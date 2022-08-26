1030c6a76a1a4278a8df3e7428bfba43687edb53
Nixonsama <samanixonn@gmail.com>
2022-08-26 00:55:38 +0000

    ripo created

---
+++ ripo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "# 1st-repo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:cloud-pro29/1st-repo.git
+git push -u origin main
